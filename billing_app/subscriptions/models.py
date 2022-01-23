2ae48cd53f83d530d84626fe6481e6d4a38cc24c
Ivan Babushkin <ivan.babushkin.home@gmail.com>
2022-01-23 16:11:34 +0300

    Add User model + migrations

--- billing_app/subscriptions/models.py
+++ billing_app/subscriptions/models.py
@@ -1,1 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    customer = models.ForeignKey(
+        "djstripe.Customer",
+        to_field="id",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    subscription = models.ForeignKey(
+        "djstripe.Subscription",
+        to_field="id",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
