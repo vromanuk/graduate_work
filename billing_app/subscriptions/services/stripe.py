23526d04c90db3fbafe414c668e639e253a6644c
Ivan Babushkin <ivan.babushkin.home@gmail.com>
2022-01-27 21:39:19 +0300

    Delete create_subscription method from StripeService (no longer required)

--- billing_app/subscriptions/services/stripe.py
+++ billing_app/subscriptions/services/stripe.py
@@ -14,12 +14,3 @@ class StripeService:
             payment_method=payment_method,
             invoice_settings={"default_payment_method": payment_method},
         )
-
-    @staticmethod
-    def create_subscription(customer_id: str, price_id: str) -> Subscription:
-        return stripe.Subscription.create(
-            customer=customer_id,
-            items=[{"price": price_id}],
-            payment_behavior="default_incomplete",
-            expand=["latest_invoice.payment_intent"],
-        )
