cbdaa2c4e1e295abaa7abe0e96effccf92d6f9a2
Ivan Babushkin <ivan.babushkin.home@gmail.com>
2022-02-01 23:39:09 +0300

    Delete payment_method parameter

--- billing_app/subscriptions/services/stripe.py
+++ billing_app/subscriptions/services/stripe.py
@@ -1,16 +1,11 @@
 import stripe
 from django.conf import settings
 from stripe.api_resources.customer import Customer
-from stripe.api_resources.subscription import Subscription
 
 
 class StripeService:
     stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
     @staticmethod
-    def create_customer(email: str, payment_method: str = None) -> Customer:
-        return stripe.Customer.create(
-            email=email,
-            payment_method=payment_method,
-            invoice_settings={"default_payment_method": payment_method},
-        )
+    def create_customer(email: str) -> Customer:
+        return stripe.Customer.create(email=email)
